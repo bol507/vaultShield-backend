421fc03a7aabdc40cf6bfd9a13c9685993c94497
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-04-25 11:25:00 -0500

    fix: message user not found

--- controllers/keypairController.js
+++ controllers/keypairController.js
@@ -4,10 +4,13 @@ const getKeyPairByUser = async (request, response) => {
     //extract user from middleware
     const user = request.user
     try{
+        if (!user) {
+            return response.status(404).json({ error: 'User not found' });
+          }
         const keypair = await KeyPair.findOne({ userId: user.id })
         .populate('user', { username: 1 });
         console.log(keypair)
-        if(keypair.length === 0 ){
+        if(!keypair ){
             response.status(404).json({error: 'Key pair not found'})
         }else{
             response.status(200).json(keypair)
