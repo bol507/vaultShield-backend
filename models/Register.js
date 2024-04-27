731f8a9f67d1f183c47d606ab20898ad0d154dd2
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-04-27 12:58:23 -0500

    feat: Register model

---
+++ models/Register.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const registerSchema = new mongoose.Schema({
+    title: {
+        type: String
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+
+})
+
+registerSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Register', registerSchema)
