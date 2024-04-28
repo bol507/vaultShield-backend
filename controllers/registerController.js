151324efedaeb636e9da6a2ce197de4fd0c95499
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-04-28 12:48:51 -0500

    feat: getRegisters

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -25,9 +25,28 @@ const createRegister = async (request, response) => {
     }catch(error){
         return response.status(500).json({error: error.message})
     }
+}
 
+const getRegisters = async (request,response) => {
+    const user = request.user;
+    try{
+        if(!user){
+            return response.status(404).json({error: 'User not found'})
+        }
+        const registers = await Register.find({ user: user._id });
+        if (!registers){
+            return response.status(404).json({error: 'Registers not found'})
+        }
+        return response.status(200).json({ registers });
+        
+
+
+    }catch(error){
+        return response.status(500).json({error: error.message})
+    }
 }
 
 module.exports = {
-    createRegister
+    createRegister,
+    getRegisters
 }
--- routes/register.js
+++ routes/register.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const registerController = require('../controllers/registerController')
 
 router.post('/',registerController.createRegister)
-
+router.get('/',registerController.getRegisters)
 module.exports = router
